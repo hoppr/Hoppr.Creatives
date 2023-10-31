5037f7f6b66806f9fae7e4ba7a1e0745fc74cc00
hopprMaxenceBourdin <74161961+Maxhoppr@users.noreply.github.com>
2023-10-31 11:51:39 +0800

    Add files via upload

---
+++ v4_sizmek_ads/Max Expandables survey/template_options.js
@@ -0,0 +1,78 @@
+// Rich Media Carousel v2
+// Help Center Guide: url TBD
+
+/****************************/
+/***** Carousel options *****/
+/****************************/
+
+/* 
+amzLoop (type : Boolean)
+- Determines if the Carousel is looping when the user clicks on previous and next arrows on the first and last slides.
+- Value : true (Carousel will loop) or false (Carousel will not loop)   
+- Default : true
+*/
+var amzLoop = true;
+
+
+/*
+amzBulletsVisibility (type : Boolean)
+- Determines if the navigation bullets are visible or not
+- Value : true (bullets are visible) or false (bullets are hidden)
+- Default : true
+*/
+var amzBulletsVisibility = true;
+
+
+/* 
+amzAutoPlay(type : Boolean)
+- Determines if the Carousel is automatically animated once initiated.
+- Value : true (turning on autoplaying) or false (turning off the autoplaying)
+- Default : true
+*/
+var amzAutoPlay = true;
+
+
+/*
+amzAutoPlayDelay (type : Integer)
+- Time spent in seconds on each slide when the Carousel is autoplaying (will be ignored if autoplay is turned off)
+- Value : numbers (e.i. 1, 2, 3...)
+- Default : 3
+*/
+var amzAutoPlayDelay = 3;
+
+
+/*
+amzDirection (type : String)
+- Determines the direction of the Carousel
+- Value : 'horizontal' (left is previous, right is next) or 'vertical' (top is previous, bottom is next)
+- Default : 'horizontal' 
+*/
+var amzDirection = 'horizontal';
+
+
+/****************************/
+/****** Design options ******/
+/****************************/
+
+/*
+amzBorder (type : String)
+- Determines the color of the 1px border around the creative, or if it should be disabled
+- Value
+	- for a color: RGB, HEX or predefined color names such as 'black' or 'white'
+	- to disable the border: 'transparent' or 'none'
+- Default : 'grey'
+*/
+var amzBorder = 'grey';
+
+
+/*
+amzButtonsColor (type : String)
+- Determines the main color of the buttons and bullets in the Carousel
+- Value : RGB, HEX or predefined color names such as 'black' or 'white'
+- Default : 'grey'
+*/
+var amzButtonsColor = 'grey';
+
+
+
+
---
+++ v4_sizmek_ads/README.txt
@@ -0,0 +1,16 @@
+Ad Format: Single Expandable
+Amazon Template Version: 1.0.1
+Last Updated On: 2023-02-20
+Version Notes:
+- remove window.addEventListener("load", checkIfAdKitReady); updated to adkit.onReady(initializeCreative);
+- remove function checkIfAdKitReady(event){...}
+- Making sure adkit url is the secure.
+
+For more details, please see the following Help Center Articles:
+AdKit: https://support.sizmek.com/hc/en-us/articles/360055240911-Load-and-Initialize-the-API
+
+Single Expandable:
+Single Expandable:
+1. About Single Expandable - https://support.sizmek.com/hc/en-us/articles/360054826372-About-Single-Expandable
+2. Create a Single Expandable in a Code Editor - https://support.sizmek.com/hc/en-us/articles/360055248051-Create-a-Single-Expandable-in-a-Code-Editor
+3. Set up a Single Expandable - https://support.sizmek.com/hc/en-us/articles/360055248071-Set-up-a-Single-Expandable
